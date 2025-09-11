00324c8276e09b8e7578e390fa6b508bc410e4d0
Dhruv Haldar <dhruv.haldar@tridiagonal.com>
2025-09-11 16:04:24 +0530

    Support for Windows

---
+++ run_openfoam_v12.ps1
@@ -0,0 +1,8 @@
+# Get current working directory in Windows PowerShell
+$CurrentDir = Get-Location
+
+# Run OpenFOAM v12 Docker container with volume mapping
+docker run --rm -it `
+    --user foam `
+    -v "${CurrentDir}:/foam/OpenFOAM/12/work" `
+    haldardhruv/ubuntu_noble_openfoam:v12
